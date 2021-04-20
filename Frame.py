84eddda5602085fec6aa0ee3aa0bc58c778c848f
SANGPHA <68215551+SANG-PHA@users.noreply.github.com>
2021-04-21 00:50:40 +0900

    -

---
+++ Frame.py
@@ -0,0 +1,34 @@
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def clear():
+    try:
+        ret_val = os.system('clear')
+        if(ret_val == True):
+            os.system('cls')
+            print('Using Window OS.')
+            print('')
+        else:
+            print("Using Linux or OSX OS")
+    except(EOFError):
+        print("Systematic Error!!!")
+
+
+def main():
+
+    (train_image, train_labels), (test_image, test_labels) = mnist.load_data()
+
+    clear()
+
+    print("Train : ", train_image.shape, "Test : ", test_image.shape)
+
+
+
+if __name__=='__main__':
+    try:
+        main()
+        os._exit(0)
+    except (EOFError, KeyboardInterrupt) :
+        print("Python doesn't work!!")
